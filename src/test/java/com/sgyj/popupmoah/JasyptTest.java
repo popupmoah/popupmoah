60acccd93e5b7bb645daaf3dfcfec84429b13abd
이승구 <leesg107@naver.com>
2025-06-15 00:32:10 +0900

    feat: jasypt 설정 추가

---
+++ src/test/java/com/sgyj/popupmoah/JasyptTest.java
@@ -0,0 +1,30 @@
+package com.sgyj.popupmoah;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class JasyptTest {
+
+    @Autowired
+    private StringEncryptor jasyptStringEncryptor;
+
+    @Test
+    @DisplayName("암복호화 테스트")
+    void encrypt () {
+        // given
+        String plainText = "Hello, Jasypt!";
+        // when
+        String encrypt = jasyptStringEncryptor.encrypt(plainText);
+        String decrypt = jasyptStringEncryptor.decrypt(encrypt);
+        // then
+        assertThat(decrypt).isEqualTo(plainText);
+
+    }
+
+}
