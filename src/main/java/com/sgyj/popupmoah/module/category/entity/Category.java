3f44b118b0242f4de54604732c9279222fc1968f
이승구 <leesg107@naver.com>
2025-06-18 16:55:40 +0900

    feat: category & popupStore 기본 entity 작성

--- src/main/java/com/sgyj/popupmoah/module/category/entity/Category.java
+++ src/main/java/com/sgyj/popupmoah/module/category/entity/Category.java
@@ -1,4 +1,24 @@
 package com.sgyj.popupmoah.module.category.entity;
 
-public class Category {
+import com.sgyj.popupmoah.infra.generator.IdGenerator;
+import com.sgyj.popupmoah.infra.jpa.UpdatedEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends UpdatedEntity {
+
+    @Id
+    @IdGenerator
+    private Long id;
+
+    private String name;
+
+    private String description;
+
 }
--- src/main/java/com/sgyj/popupmoah/module/popupstore/entity/PopupStore.java
+++ src/main/java/com/sgyj/popupmoah/module/popupstore/entity/PopupStore.java
@@ -1,4 +1,29 @@
 package com.sgyj.popupmoah.module.popupstore.entity;
 
-public class PopupStore {
+import com.sgyj.popupmoah.infra.generator.IdGenerator;
+import com.sgyj.popupmoah.infra.jpa.Location;
+import com.sgyj.popupmoah.infra.jpa.UpdatedEntity;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PopupStore extends UpdatedEntity {
+
+    @Id
+    @IdGenerator
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Embedded
+    private Location location;
+
 }
