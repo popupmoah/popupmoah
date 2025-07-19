1759ed818ed1131ad5add805f65d95355d9dee4f
seunggu lee <leesg107@naver.com>
2025-07-19 12:42:11 +0900

    fix: javax -> jakarta 변경

--- src/main/java/com/sgyj/popupmoah/module/community/controller/request/CreateReviewRequest.java
+++ src/main/java/com/sgyj/popupmoah/module/community/controller/request/CreateReviewRequest.java
@@ -2,10 +2,11 @@ package com.sgyj.popupmoah.module.community.controller.request;
 
 import lombok.Getter;
 import lombok.Setter;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Max;
+// javax.validation → jakarta.validation 으로 변경 (Java 21/Spring Boot 3.x)
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 
 @Getter
 @Setter
