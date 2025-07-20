f7353495d4c7ada70e9bf3d29d2ba96b1eb17f79
이승구 <leesg107@naver.com>
2025-07-20 16:31:05 +0900

    fix: javax -> jakarta 변경

--- src/main/java/com/sgyj/popupmoah/module/community/controller/request/UpdateCommentRequest.java
+++ src/main/java/com/sgyj/popupmoah/module/community/controller/request/UpdateCommentRequest.java
@@ -2,7 +2,6 @@ package com.sgyj.popupmoah.module.community.controller.request;
 
 import lombok.Getter;
 import lombok.Setter;
-// javax.validation → jakarta.validation 으로 변경 (Java 21/Spring Boot 3.x)
 import jakarta.validation.constraints.NotBlank;
 
 @Getter
